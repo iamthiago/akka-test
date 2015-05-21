c4783337d79e3a20b0d7ae066b2d80a4a19d88ba
Thiago Pereira <thiagoandrade6@gmail.com>
2015-05-20 21:16:31 -0300

    testing akka with a pool and countdown

--- src/test/scala/com/akka/test/kafka/KafkaActor.scala
+++ src/test/scala/com/akka/test/kafka/KafkaActor.scala
@@ -1,49 +1,83 @@
 package com.akka.test.kafka
 
 import java.util.Properties
+import java.util.concurrent.CountDownLatch
 
 import akka.actor.SupervisorStrategy.{Escalate, Restart, Stop}
 import akka.actor._
+import akka.routing.RoundRobinPool
 import kafka.common.{FailedToSendMessageException, NoBrokersForPartitionException}
 import kafka.producer.{KeyedMessage, Producer, ProducerClosedException, ProducerConfig}
 import org.apache.kafka.common.KafkaException
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 /**
  * Created by thiago on 5/15/15.
  */
-class KafkaActor(config: ProducerConfig) extends Actor {
-
-  override val supervisorStrategy =
-    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1.minute) {
-      case _: ActorInitializationException => Stop
-      case _: FailedToSendMessageException => Restart
-      case _: ProducerClosedException => Restart
-      case _: NoBrokersForPartitionException => Escalate
-      case _: KafkaException => Escalate
-      case _: Exception => Escalate
-    }
-
-  private val producer = new Producer[String, String](config)
-
-  override def postStop(): Unit = producer.close()
-
-  def receive = {
-    case x: Any => producer.send(new KeyedMessage[String, String]("test", "thiago"))
-  }
-}
-
 object Test extends App {
-  val props = new Properties()
-  props.put("metadata.broker.list", "localhost:9092")
-  props.put("serializer.class", "kafka.serializer.StringEncoder")
-  props.put("request.required.acks", "1")
+
+  val latch = new CountDownLatch(10)
+
+  class KafkaActor extends Actor {
+
+    override val supervisorStrategy =
+      OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1.minute) {
+        case _: ActorInitializationException => Stop
+        case _: FailedToSendMessageException => Restart
+        case _: ProducerClosedException => Restart
+        case _: NoBrokersForPartitionException => Escalate
+        case _: KafkaException => Escalate
+        case _: Exception => Escalate
+      }
+
+    val props = new Properties()
+    props.put("metadata.broker.list", "localhost:9092")
+    props.put("serializer.class", "kafka.serializer.DefaultEncoder")
+    props.put("request.required.acks", "-1")
+    props.put("producer.type", "sync")
+    props.put("compression.codec", "gzip")
+
+    private val producer = new Producer[String, Array[Byte]](new ProducerConfig(props))
+
+    override def postStop(): Unit = producer.close()
+
+    val random = new Random()
+
+    def receive = {
+      case x: Any =>
+        /*val startTime = System.currentTimeMillis()*/
+        val bytes = Array.fill[Byte](100000)((random.nextInt() % 26 + 'a').toByte)
+
+        for (i <- 1 to 100) {
+          producer.send(new KeyedMessage[String, Array[Byte]]("my-topic", bytes))
+        }
+
+        latch.countDown()
+
+        /*val finishTime = System.currentTimeMillis()
+        val millis = Duration.create(finishTime - startTime, "millis")
+        println("time in " + millis)
+        println("time in " + millis.toSeconds + " seconds")*/
+    }
+  }
 
   val system = ActorSystem("KafkaSystem")
-  val kafkaActor = system.actorOf(Props(new KafkaActor(new ProducerConfig(props))), name = "helloactor")
+
+  val kafkaActor = system.actorOf(Props[KafkaActor].withRouter(RoundRobinPool(4)), name = "helloactor")
+
+  val startTime = System.currentTimeMillis()
 
   for (i <- 1 to 10) {
     kafkaActor ! "bla"
   }
+
+  latch.await()
+
+  val finishTime = System.currentTimeMillis()
+  val millis = Duration.create(finishTime - startTime, "millis")
+  println("time in " + millis)
+  println("time in " + millis.toSeconds + " seconds")
+
 }
